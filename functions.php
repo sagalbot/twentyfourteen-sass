c9774cf83d780aa04b7c4ce4888f4d3f10e5ae1f
Jeff Sagal <sagalbot@gmail.com>
2014-02-24 12:36:59 -0800

    added icon font, fixed mobile nav toggle, added icons to nav

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
 function sagalbot_scripts() {
 
 	wp_enqueue_script( 'sagalbot-script', get_stylesheet_directory_uri() . '/js/app.min.js', array('jquery'), false, true );
+	wp_localize_script( 'sagalbot-script', 'sagalbot', array(
+			'url'	=>	get_home_url()
+		) );
 
 }
 add_action('wp_enqueue_scripts', 'sagalbot_scripts');
--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function($) {
 	
-	$('a[href="http://code.sagalbot.com"]').prepend('<i class="code-sagalbot genericon genericon-code"></i>').parent('li').addClass('code-sagalbot');
+
+	//  code on sagalbot
+	$('#primary-navigation a[href="http://code.sagalbot.com"]').prepend('<i class="code-sagalbot genericon genericon-code"></i>').parent('li').addClass('code-sagalbot');
+	
+	// code on code.sagalbot
+	$('.site-title a[rel="home"]').append('<i class="code-sagalbot genericon genericon-code"></i>');
+
+	//  Photo Icon
+	$('#primary-navigation a[href="'+sagalbot.url+'/category/photography/"]').prepend('<i class="genericon genericon-image"></i>');
+
+	//  Snow Icon
+	$('#primary-navigation a[href="'+sagalbot.url+'/category/boards/"]').prepend('<i class="icon-fontello icon-snow"></i>');
+
+	//  Sun Icon
+	$('#primary-navigation a[href="'+sagalbot.url+'/category/bikes/"]').prepend('<i class="icon-fontello icon-sun"></i>');
 
 });
