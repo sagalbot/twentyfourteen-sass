b9c6e5da630652e5048d1c802a2101682ebb1429
Jeff Sagal <sagalbot@gmail.com>
2014-02-24 00:46:26 -0800

    fixed header colors, added code icon

---
+++ js/app.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+	
+	$('a[href="http://code.sagalbot.com"]').prepend('<i class="code-sagalbot genericon genericon-code"></i>').parent('li').addClass('code-sagalbot');
+
+});
